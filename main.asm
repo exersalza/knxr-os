8426bb2edc454816e67bcd78f263a2f81d70fe2f
exersalza <squidodle44@gmail.com>
2022-05-02 13:21:16 +0200

    added Bootloader, which only prints hello world but it works

---
+++ main.asm
@@ -0,0 +1,26 @@
+[org 0x7c00]
+
+mov si, STR
+call printf
+
+jmp $
+
+printf:
+	pusha
+	str_loop:
+		mov al, [si]
+		cmp al, 0
+		jne print_char
+		popa
+		ret
+
+	print_char:
+		mov ah, 0x0e
+		int 0x10
+		add si, 1
+		jmp str_loop
+
+STR: db "Hello World!", 0
+
+times 510-($-$$) db 0
+dw 0xaa55
